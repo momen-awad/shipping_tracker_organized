52d425a8c8f85bed04c194a412f6a363d84e1650
Mo'men awad Muhammed <69948824+momen-awad@users.noreply.github.com>
2025-05-21 11:10:56 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0A2463",
+        secondary: "#4ECDC4",
+        accent: "#FF7F11",
+      },
+    },
+  },
+  plugins: [],
+}
